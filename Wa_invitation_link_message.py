9f296cc002217fe0c1ec799768a3f0029c345fd3
ShoeraRei <70783940+ShoeraRei@users.noreply.github.com>
2020-09-05 12:46:01 +0700

    Add files via upload
    Fix error str

--- Wa_invitation_link_message.py
+++ Wa_invitation_link_message.py
@@ -2,6 +2,6 @@ print("script by ShoeraRei");
 wa_link = "https://wa.me/";
 number_phone = int(input ("enter phone number without \"+\" :"));
 message = input("your message :");
-join = wa_link+number_phone+"?text="+message.replace(" ","%20");
+join = wa_link+str(number_phone)+"?text="+message.replace(" ","%20");
 
 print("copy this limk \n"+join);
