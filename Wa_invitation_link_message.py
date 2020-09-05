4cd5035c64b2b8ac761f725cba6d4ad3a76aba2c
ShoeraRei <70783940+ShoeraRei@users.noreply.github.com>
2020-09-05 08:40:45 +0700

    Add files via upload
    The script is v 1.0

---
+++ Wa_invitation_link_message.py
@@ -0,0 +1,7 @@
+print("script by ShoeraRei");
+wa_link = "https://wa.me/";
+number_phone = int(input ("enter phone number without \"+\" :"));
+message = input("your message :");
+join = wa_link+number_phone+"?text="+message.replace(" ","%20");
+
+print("copy this limk \n"+join);
